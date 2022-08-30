869dbcd5c9daece2c8fa42a531ca9d0c1b78b590
Healsu <timmie2500@gmail.com>
2022-08-30 09:30:51 +0200

    Initial commit

---
+++ src/main/java/com/example/funkywebsiteforcoolkidsonly/FunkyWebsiteForCoolKidsOnlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.funkywebsiteforcoolkidsonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkyWebsiteForCoolKidsOnlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunkyWebsiteForCoolKidsOnlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/funkywebsiteforcoolkidsonly/FunkyWebsiteForCoolKidsOnlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funkywebsiteforcoolkidsonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunkyWebsiteForCoolKidsOnlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
